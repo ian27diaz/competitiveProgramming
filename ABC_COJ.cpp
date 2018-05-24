066b0fcee1577f1cec10a6c84880d98d5613ab5c
Ian Diaz Meda <35357934+ian27diaz@users.noreply.github.com>
2018-05-24 12:47:58 -0500

    Add files via upload

---
+++ ABC_COJ.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+inline void swap(int arr[], int posA, int posB){
+	int swap = arr[posA];
+	arr[posA] = arr[posB];
+	arr[posB] = swap;
+}
+void sortArray(int arr[]){
+	if(arr[0] > arr[1]) swap(arr, 0, 1);
+	if(arr[1] > arr[2]) swap(arr, 1, 2);
+	if(arr[0] > arr[1]) swap(arr, 0, 1);
+}
+
+int main(){
+	
+	int arr[3];
+	cin >> arr[0] >> arr[1] >> arr[2];
+	sortArray(arr);
+	string str;
+	cin >> str;
+	if(str == "ABC"){
+		cout << arr[0] << " " << arr[1] << " " << arr[2];
+	}
+	if(str == "ACB"){
+		cout << arr[0] << " " << arr[2] << " " << arr[1];	
+	}
+	if(str == "BAC"){
+		cout << arr[1] << " " << arr[0] << " " << arr[2];	
+	}
+	if(str == "BCA"){
+		cout << arr[1] << " " << arr[2] << " " << arr[0];	
+	}
+	if(str == "CAB"){
+		cout << arr[2] << " " << arr[0] << " " << arr[1];	
+	}
+	if(str == "CBA"){
+		cout << arr[2] << " " << arr[1] << " " << arr[0];	
+	}
+	
+	return 0;
+}
