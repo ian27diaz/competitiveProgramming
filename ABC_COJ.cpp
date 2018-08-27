66bfa8d64d7fa461a37a40afb33f8d569bad0bee
Isaac2357 <35746413+Isaac2357@users.noreply.github.com>
2018-08-27 10:24:18 -0500

    Update ABC_COJ.cpp

--- ABC_COJ.cpp
+++ ABC_COJ.cpp
@@ -8,6 +8,7 @@ inline void swap(int arr[], int posA, int posB){
 	arr[posB] = swap;
 }
 void sortArray(int arr[]){
+	//Sort array
 	if(arr[0] > arr[1]) swap(arr, 0, 1);
 	if(arr[1] > arr[2]) swap(arr, 1, 2);
 	if(arr[0] > arr[1]) swap(arr, 0, 1);
